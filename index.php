c559aacb1f98a7953f3c2aab8867d354306a628c
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2018-07-24 11:30:34 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Photo Sharing app</title>
+    </head>
+    <body>
+        <div id="top">
+            <h1>Photo Sharing App</h1>
+        </div>
+        
+    </body>
+</html>
